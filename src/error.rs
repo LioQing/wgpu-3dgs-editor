465cbd8808fc1a863f3192972fbb6d77deb90d5d
LioQing <s2010157@gmail.com>
2025-07-08 00:16:02 +0800

    Fix selection builder

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,11 @@
 use thiserror::Error;
 
+use crate::core;
+
 #[derive(Debug, Error)]
 pub enum Error {
+    #[error("{0}")]
+    Core(#[from] core::Error),
     #[error("{0}")]
     BufferDownloadOneShotReceive(#[from] oneshot::RecvError),
     #[error("{0}")]
