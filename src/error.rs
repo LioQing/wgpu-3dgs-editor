fda25a6e5c10baf242367fed595c10f5526a4ca6
LioQing <s2010157@gmail.com>
2025-07-16 22:10:08 +0800

    Add selection and buffer expression

--- src/error.rs
+++ src/error.rs
@@ -6,10 +6,4 @@ use crate::core;
 pub enum Error {
     #[error("{0}")]
     Core(#[from] core::Error),
-    #[error("{0}")]
-    BufferDownloadOneShotReceive(#[from] oneshot::RecvError),
-    #[error("{0}")]
-    BufferDownloadAsync(#[from] wgpu::BufferAsyncError),
-    #[error("{0}")]
-    DeviceFailedToPoll(#[from] wgpu::PollError),
 }
