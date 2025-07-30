f04c226a7971d6df2cdbd0e5234cfe0c0e4acc7d
LioQing <s2010157@gmail.com>
2025-07-30 22:46:47 +0800

    Add editor

--- src/lib.rs
+++ src/lib.rs
@@ -16,31 +16,26 @@ pub use selection_modifier::*;
 pub use wgpu_3dgs_core as core;
 
 use wgpu_3dgs_core::{
-    ComputeBundle, GaussianPod, GaussianTransformBuffer, Gaussians, GaussiansBuffer,
-    ModelTransformBuffer,
+    GaussianPod, GaussianTransformBuffer, Gaussians, GaussiansBuffer, ModelTransformBuffer,
 };
 
-/// The basic editor.
-#[derive(Debug)]
-pub struct BasicEditor<G: GaussianPod> {
+/// An editor for Gaussians.
+///
+/// This enables the application of a sequence of [`Modifier`]s to the Gaussians.
+pub struct Editor<G: GaussianPod> {
     pub model_transform_buffer: ModelTransformBuffer,
     pub gaussian_transform_buffer: GaussianTransformBuffer,
     pub gaussians_buffer: GaussiansBuffer<G>,
-    pub selection_buffer: SelectionBuffer,
-    pub transform_flags_buffer: TransformFlagsBuffer,
-    pub basic_color_modifiers_buffer: BasicColorModifiersBuffer,
-    pub scale_rotation_buffer: ScaleRotationBuffer,
 
-    pub selection: SelectionBundle,
-    pub modifiers: BasicModifiersBundle,
+    pub modifiers: Vec<Box<dyn Modifier<G>>>,
 }
 
-impl<G: GaussianPod> BasicEditor<G> {
+impl<G: GaussianPod> Editor<G> {
     /// Create a new basic editor.
     pub fn new(
         device: &wgpu::Device,
-        selection_ops: Vec<ComputeBundle<()>>,
         gaussians: &Gaussians,
+        modifiers: impl IntoIterator<Item = impl Into<Box<dyn Modifier<G>>>>,
     ) -> Self {
         log::debug!("Creating model transform buffer");
         let model_transform_buffer = ModelTransformBuffer::new(device);
@@ -51,45 +46,27 @@ impl<G: GaussianPod> BasicEditor<G> {
         log::debug!("Creating gaussians buffer");
         let gaussians_buffer = GaussiansBuffer::new(device, &gaussians.gaussians);
 
-        log::debug!("Creating selection buffer");
-        let selection_buffer = SelectionBuffer::new(device, gaussians.gaussians.len() as u32);
-
-        log::debug!("Creating transform flags buffer");
-        let transform_flags_buffer = TransformFlagsBuffer::new(device);
-
-        log::debug!("Creating basic color modifiers buffer");
-        let basic_color_modifiers_buffer = BasicColorModifiersBuffer::new(device);
-
-        log::debug!("Creating scale rotation buffer");
-        let scale_rotation_buffer = ScaleRotationBuffer::new(device);
-
-        log::debug!("Creating selection bundle");
-        let selection = SelectionBundle::new::<G>(device, selection_ops);
-
-        log::debug!("Creating basic modifiers bundle");
-        let modifiers = BasicModifiersBundle::new(
-            device,
-            &gaussians_buffer,
-            &model_transform_buffer,
-            &gaussian_transform_buffer,
-            &transform_flags_buffer,
-            &basic_color_modifiers_buffer,
-            &scale_rotation_buffer,
-        );
-
         log::debug!("Basic editor created");
 
         Self {
             model_transform_buffer,
             gaussian_transform_buffer,
             gaussians_buffer,
-            selection_buffer,
-            transform_flags_buffer,
-            basic_color_modifiers_buffer,
-            scale_rotation_buffer,
 
-            selection,
-            modifiers,
+            modifiers: modifiers.into_iter().map(Into::into).collect(),
+        }
+    }
+
+    /// Apply the modifiers to the Gaussians.
+    pub fn apply(&self, device: &wgpu::Device, encoder: &mut wgpu::CommandEncoder) {
+        for modifier in &self.modifiers {
+            modifier.apply(
+                device,
+                encoder,
+                &self.gaussians_buffer,
+                &self.model_transform_buffer,
+                &self.gaussian_transform_buffer,
+            );
         }
     }
 }
